6d4f0fc5b0ff64fd2887c0b8643b7f3b66d8e716
lucas <llato@wisc.edu>
2016-03-11 16:32:34 -0600

    database work

---
+++ app/src/main/java/com/example/lucas/homework2/DaoGenerator.java
@@ -0,0 +1,33 @@
+package com.example.lucas.homework2;
+
+/**
+ * Created by Lucas on 3/11/2016.
+ */
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+
+public class DaoGenerator {
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1, "com.example.lucas.homework2"); //Scheme for GreenDAO ORM
+        createDB(schema);
+        new DaoGenerator().generateAll(schema, "./app/src/main/java/");
+        //where you want to store the generated classes.
+    }
+
+    private static void createDB(Schema schema) {
+
+        //Events
+        Entity event = schema.addEntity("Event");
+        event.addIdProperty();
+        event.addStringProperty("name");
+        event.addStringProperty("location");
+        event.addStringProperty("startTime");
+        event.addStringProperty("endTime");
+        event.addStringProperty("date");
+        event.addBooleanProperty("display");
+
+    }
+
+}
